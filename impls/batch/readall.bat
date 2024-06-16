395daf4f2bbaf5d9b4e39c25f150008e9e954926
老刘 <632171029@qq.com>
2024-06-16 11:29:35 +0800

    batch: add readall.bat & add intro info of readline.bat

---
+++ impls/batch/readall.bat
@@ -0,0 +1,16 @@
+@REM Will read to eof and return escaped string.
+@REM #$D$# is added to the beginning and end of each line.
+
+@REM Special Symbol Mapping:
+@REM 	! --- #$E$#
+@REM 	^ --- #$C$#
+@REM 	" --- #$D$#
+@REM 	% --- #$P$#
+
+@echo off
+setlocal disabledelayedexpansion
+
+for /f "tokens=* eol=" %%a in ('more') do (
+	echo "%%a"|readline.bat
+)
+goto :eof
