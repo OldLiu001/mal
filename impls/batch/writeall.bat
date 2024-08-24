01b9df1905c87b71d96b08d72c532be051ee427d
老刘 <632171029@qq.com>
2024-08-24 22:44:54 +0800

    batch: update `special symbol mapping` to avoid ambiguous

--- impls/batch/writeall.bat
+++ impls/batch/writeall.bat
@@ -1,10 +1,11 @@
 @REM Read escaped string from stdin and output unescaped string.
 
 @REM Special Symbol Mapping:
-@REM 	! --- #$E$#
-@REM 	^ --- #$C$#
-@REM 	" --- #$D$#
-@REM 	% --- #$P$#
+@REM 	! --- $E
+@REM 	^ --- $C
+@REM 	" --- $D
+@REM 	% --- $P
+@REM 	$ --- $$
 
 @echo off & setlocal ENABLEDELAYEDEXPANSION
 
@@ -13,17 +14,21 @@ for /f "delims=" %%i in ('more') do (
 	rem replace all speical symbol back.
 	set OutputBuffer=
 	:LOCALTAG_Print_OutputLoop
-	if "!Output:~,5!" == "#$E$#" (
+	if "!Output:~,2!" == "$$" (
+		set "OutputBuffer=!OutputBuffer!$"
+		set "Output=!Output:~2!"
+		goto LOCALTAG_Print_OutputLoop
+	) else if "!Output:~,2!" == "$E" (
 		set "OutputBuffer=!OutputBuffer!^!"
-		set "Output=!Output:~5!"
+		set "Output=!Output:~2!"
 		goto LOCALTAG_Print_OutputLoop
-	) else if "!Output:~,5!" == "#$C$#" (
+	) else if "!Output:~,2!" == "$C" (
 		set "OutputBuffer=!OutputBuffer!^^"
-		set "Output=!Output:~5!"
+		set "Output=!Output:~2!"
 		goto LOCALTAG_Print_OutputLoop
-	) else if "!Output:~,5!" == "#$D$#" (
+	) else if "!Output:~,2!" == "$D" (
 		set "OutputBuffer=!OutputBuffer!^""
-		set "Output=!Output:~5!"
+		set "Output=!Output:~2!"
 		goto LOCALTAG_Print_OutputLoop
 	) else if "!Output:~,1!" == "=" (
 		set "OutputBuffer=!OutputBuffer!="
@@ -33,9 +38,9 @@ for /f "delims=" %%i in ('more') do (
 		set "OutputBuffer=!OutputBuffer! "
 		set "Output=!Output:~1!"
 		goto LOCALTAG_Print_OutputLoop
-	) else if "!Output:~,5!" == "#$P$#" (
+	) else if "!Output:~,2!" == "$P" (
 		set "OutputBuffer=!OutputBuffer!%%"
-		set "Output=!Output:~5!"
+		set "Output=!Output:~2!"
 		goto LOCALTAG_Print_OutputLoop
 	) else if defined Output (
 		set "OutputBuffer=!OutputBuffer!!Output:~,1!"
