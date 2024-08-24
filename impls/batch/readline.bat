b384f744c7a8f52d3d3b5c397b2e7c71d8c517e5
老刘 <632171029@qq.com>
2024-08-24 22:44:54 +0800

    batch: update `special symbol mapping` to avoid ambiguous

--- impls/batch/readline.bat
+++ impls/batch/readline.bat
@@ -1,10 +1,11 @@
 @REM Will read a line from stdin and return escaped string.
 
 @REM Special Symbol Mapping:
-@REM 	! --- #$E$#
-@REM 	^ --- #$C$#
-@REM 	" --- #$D$#
-@REM 	% --- #$P$#
+@REM 	! --- $E
+@REM 	^ --- $C
+@REM 	" --- $D
+@REM 	% --- $P
+@REM 	$ --- $$
 
 @echo off
 setlocal disabledelayedexpansion
@@ -12,20 +13,22 @@ setlocal disabledelayedexpansion
 set Input=
 set /p "Input="
 if defined Input (
-	rem first replace double quotation mark.
-	set "Input=%Input:"=#$D$#%"
+	rem First, replace $ to $$.
+	set "Input=%Input:$=$$%"
+	rem Replace double quotation mark.
+	set "Input=%Input:"=$D%"
 	rem Batch can't deal with "!" when delayed expansion is enabled, so replace it to a special string.
-	call set "Input=%%Input:!=#$E$#%%"
+	call set "Input=%%Input:!=$E%%"
 	setlocal ENABLEDELAYEDEXPANSION
 	%Speed Improve Start% (
 		rem Batch has some problem in "^" processing, so replace it.
-		set "Input=!Input:^=#$C$#!"
-		rem replace %.
+		set "Input=!Input:^=$C!"
+		rem Replace %.
 		set FormatedInput=
 		:LOCALTAG_Main_ReplacementLoop
 		if defined Input (
 			if "!Input:~,1!" == "%%" (
-				set "FormatedInput=!FormatedInput!#$P$#"
+				set "FormatedInput=!FormatedInput!$P"
 			) else (
 				set "FormatedInput=!FormatedInput!!Input:~,1!"
 			)
