cbe509b290a2ea90faa59cfdc78b7b66a656f1a8
老刘 <632171029@qq.com>
2024-08-25 13:58:18 +0800

    add `PrepareCall` `GetArgs`

--- impls/batch/function.bat
+++ impls/batch/function.bat
@@ -4,7 +4,10 @@
 @rem Export Functions:
 @REM 	:SaveCurrentCallInfo _Name
 @REM 	:RestoreCallInfo
-
+@REM 	:PrepareCall _ArgName1 _ArgName2 ...
+@REM 	:GetArgs _ParaName1 _ParaName2 ...
+@REM 	:Return _RetVal
+@REM 	:GetRetVal _VarName
 
 @rem Used Namespaces:
 @rem 	G_CallPath
@@ -29,5 +32,13 @@ goto :eof
 
 	:RestoreCallInfo
 		call Stackframe.bat :GetVars G_CallPath
+	goto :eof
+
+	:PrepareCall _ArgName1 _ArgName2 ...
+		call Stackframe.bat :SaveVars %*
+	goto :eof
+
+	:GetArgs _ParaName1 _ParaName2 ...
+		call Stackframe.bat :GetVars %*
 	goto :eof
 ) % Module - Function - End %
