048b62ebf63c542ba32b5fdedc47d2ec3f90156f
老刘 <632171029@qq.com>
2024-08-27 13:24:17 +0800

    Update sf.bat
    +SaveLocalVars
    +RestoreLocalVars
    -SaveVars
    -GetVars

--- impls/batch/sf.bat
+++ impls/batch/sf.bat
@@ -1,10 +1,3 @@
-@rem Module Name: Stackframe
-
-@rem Export Functions:
-@rem 	:SaveVars _VarName1 _VarName2 ...
-@rem 	:GetVars  _VarName1 _VarName2 ...
-
-
 @echo off
 
 ::Start
@@ -41,49 +34,23 @@ goto :eof
 	set /a G_SP -= 1
 goto :eof
 
-:GetVars _VarName1 _VarName2 ...
-	rem _VarName is different from each other.
-	set "_VarList=%*"
-	
-	rem Pop Vars.
-	call :PopVar _VarCount
-	for /l %%i in (1 1 !_VarCount!) do (
-		call :PopVar _VarName
-
-		rem check if _VarName in _VarList.
-		for %%j in (!_VarName!) do (
-			if "!_VarList!" == "!_VarList:%%j=!" (
-				>&2 echo _VarName: %%j is not in _VarList.
-				pause & exit 1
-			)
-
-			rem Remove _VarName from _VarList.
-			set _VarList=!_VarList:%%j=!
-		)
-		call :PopVar !_VarName!
-	)
-	rem check if _VarList is empty.
-	for %%_ in (!_VarList!) do (
-		>&2 echo Need more Vars: !_VarList!
-		pause & exit 1
-	)
-goto :eof
-
-:SaveVars _VarName1 _VarName2 ...
-	rem _VarName is different from each other.
-	set "_VarList=%*"
-	
-	rem Count Var number.
-	set "_VarCount=0"
-	for %%i in (!_VarList!) do (
-		if not defined %%i (
-			>&2 echo VarName: %%i is not defined.
-			pause & exit 1
-		)
+:SaveLocalVars
+	set "G_TMP=0"
+	for /f "delims==" %%i in ('set _ 2^>nul') do (
 		call :PushVar %%i
 		call :PushVal %%i
-		set /a _VarCount += 1
+		set /a G_TMP += 1
 	)
-	rem Push Var count.
-	call :PushVal !_VarCount!
+	call :PushVal !G_TMP!
+	set "G_TMP="
+goto :eof
+
+:RestoreLocalVars
+	call :PopVar G_TMP_COUNT
+	for /l %%i in (1 1 !G_TMP_COUNT!) do (
+		call :PopVar G_TMP_VARNAME
+		call :PopVar !G_TMP_VARNAME!
+	)
+	set "G_TMP_COUNT="
+	set "G_TMP_VARNAME="
 goto :eof
