d9762df5409975d48be4e5af4e079ecefbc89ee9
老刘 <632171029@qq.com>
2024-08-27 16:13:12 +0800

    Update step0_repl.bat

--- impls/batch/step0_repl.bat
+++ impls/batch/step0_repl.bat
@@ -1,40 +1,64 @@
 @echo off
 pushd "%~dp0"
 setlocal ENABLEDELAYEDEXPANSION
+call :Invoke :Main
+exit /b 0
+
 
 :Main
 	set "_Prompt=user> "
-	call IO.bat :WriteVar _Prompt
-	call IO.bat :ReadEscapedLine
-	call :REP G_RET
+	call :Invoke IO.bat :WriteVar _Prompt
+	call :Invoke IO.bat :ReadEscapedLine
+	set "_Input=!G_RET!"
+	call :Invoke :REP _Input
+
+	set "G_RET="
 	call :ClearLocalVars
 goto :Main
 
 :Read _MalCode
-	set "G_RET=!%~1!"
+	set "_MalCode=!%~1!"
+
+	set "G_RET=!_MalCode!"
 	call :ClearLocalVars
 goto :eof
 
 :Eval _MalCode
-	set "G_RET=!%~1!"
+	set "_MalCode=!%~1!"
+
+	set "G_RET=!_MalCode!"
 	call :ClearLocalVars
 goto :eof
 
 :Print _MalCode
 	set "_MalCode=!%~1!"
-	call IO.bat :WriteEscapedLineVar _MalCode
+
+	call :Invoke IO.bat :WriteEscapedLineVar _MalCode
+
 	set "G_RET="
 	call :ClearLocalVars
 goto :eof
 
 :REP _MalCode
 	set "_MalCode=!%~1!"
-	call :READ _MalCode
-	call :EVAL G_RET
-	call :PRINT G_RET
+	
+	call :Invoke :READ _MalCode
+	set "_MalCode=!G_RET!"
+	call :Invoke :EVAL _MalCode
+	set "_MalCode=!G_RET!"
+	call :Invoke :PRINT _MalCode
+
 	call :ClearLocalVars
 goto :eof
 
-:ClearLocalVars
-	for /f "delims==" %%a in ('set _ 2^>nul') do set "%%a="
-goto :eof
+(
+	:Invoke
+		call SF.Bat :SaveLocalVars
+		call %*
+		call SF.Bat :RestoreLocalVars
+	goto :eof
+
+	:ClearLocalVars
+		for /f "delims==" %%a in ('set _ 2^>nul') do set "%%a="
+	goto :eof
+)
