0df42dbd593589399874b46a749451de3c45bc60
老刘 <632171029@qq.com>
2024-11-15 13:52:06 +0800

    Update step0_repl.bat

--- impls/batch/step0_repl.bat
+++ impls/batch/step0_repl.bat
@@ -3,7 +3,7 @@ if "%~1" equ "CALL_SELF" (
 	for /f "tokens=1,*" %%a in ('echo.%*') do (
 		call %%b || %?|% "Call '%~nx0' failed."
 	)
-	exit /b 0
+	%-|%
 )
 pushd "%~dp0"
 setlocal ENABLEDELAYEDEXPANSION
@@ -13,52 +13,52 @@ if not defined MAL_BATCH_IMPL_SINGLE_FILE (
 	call :UTILITIES_Init %~n0
 )
 
-!_C_Invoke! MAIN Main
-exit /b 0
+%|% MAIN Main
+%-|%
 
 :MAIN_Main
 	for %%. in (_L{!_G_LEVEL!}_) do (
 		for /l %%_ in () do (
-			set "%%.Prompt=user> " & !_C_Invoke! IO WriteVar %%.Prompt
-			!_C_Invoke! IO ReadEscapedLine
+			set "%%.Prompt=user> " & %|% IO WriteVar %%.Prompt
+			%|% IO ReadEscapedLine
 			if defined _G_RET (
-				!_C_GetRet! %%.Input
+				%|->% %%.Input
 			) else (
 				goto :Main
 			)
-			!_C_Invoke! MAIN REP %%.Input
+			%|% MAIN REP %%.Input
 		)
 	)
-exit /b 0
+%-|%
 
 :MAIN_Read _Mal -> _Mal
 	for %%. in (_L{!_G_LEVEL!}_) do (
 		set "%%.Mal=!%~1!"
-		!_C_Return! %%.Mal
+		%<-% %%.Mal
 	)
-exit /b 0
+%-|%
 
 :MAIN_Eval _Mal -> _Mal
 	for %%. in (_L{!_G_LEVEL!}_) do (
 		set "%%.Mal=!%~1!"
-		!_C_Return! %%.Mal
+		%<-% %%.Mal
 	)
-exit /b 0
+%-|%
 
 :MAIN_Print _Mal -> _
 	for %%. in (_L{!_G_LEVEL!}_) do (
 		set "%%.Mal=!%~1!"
-		!_C_Invoke! IO WriteEscapedLineVar %%.Mal
-		!_C_Return! _
+		%|% IO WriteEscapedLineVar %%.Mal
+		%<-% _
 	)
-exit /b 0
+%-|%
 
 :MAIN_REP _Mal -> _
 	for %%. in (_L{!_G_LEVEL!}_) do (
 		set "%%.Mal=!%~1!"
-		!_C_Invoke! MAIN Read %%.Mal & !_C_GetRet! %%.Mal
-		!_C_Invoke! MAIN Eval %%.Mal & !_C_GetRet! %%.Mal
-		!_C_Invoke! MAIN Print %%.Mal
-		!_C_Return! _
+		%|% MAIN Read %%.Mal %->% %%.Mal
+		%|% MAIN Eval %%.Mal %->% %%.Mal
+		%|% MAIN Print %%.Mal
+		%<-% _
 	)
-exit /b 0
+%-|%
