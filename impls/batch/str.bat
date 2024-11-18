90ef9ec5923998fd487ddbcde261e8286ee7e1a5
老刘 <632171029@qq.com>
2024-11-18 14:13:08 +0800

    Update str.bat

--- impls/batch/str.bat
+++ impls/batch/str.bat
@@ -4,86 +4,86 @@ if "%~1" neq "" (
 )
 %-|%
 
-:STR_New -> _Str
+:STR_New -> Str
 	for %%. in (_L{!_G_LEVEL!}_) do (
-		!_C_Invoke! NS New String & !_C_GetRet! %%.Str
+		%|% NS New String %->% %%.Str
 		set "!%%.Str!.LineCount=0"
-		!_C_Return! %%.Str
+		%<-% %%.Str
 	)
-exit /b 0
+%-|%
 
-:STR_FromVar _Var -> _Str
+:STR_FromVar _Var -> Str
 	for %%. in (_L{!_G_LEVEL!}_) do (
-		!_C_Invoke! NS New String & !_C_GetRet! %%.Str
+		%|% NS New String %->% %%.Str
 		set "!%%.Str!.LineCount=1"
 		set "!%%.Str!.Line[1]=!%~1!"
-		!_C_Return! %%.Str
+		%<-% %%.Str
 	)
-exit /b 0
+%-|%
 
-:STR_FromVal _Val -> _Str
+:STR_FromVal _Val -> Str
 	for %%. in (_L{!_G_LEVEL!}_) do (
-		!_C_Invoke! NS New String & !_C_GetRet! %%.Str
+		%|% NS New String %->% %%.Str
 		set "!%%.Str!.LineCount=1"
 		set "!%%.Str!.Line[1]=%~1"
-		!_C_Return! %%.Str
+		%<-% %%.Str
 	)
-exit /b 0
+%-|%
 
-:STR_AppendStr _Str _NewStr -> _
+:STR_AppendStr &Str &NewStr
 	for %%. in (_L{!_G_LEVEL!}_) do (
-		!_C_Copy! !%~1!.LineCount %%.LineCount
-		!_C_Copy! !%~2!.LineCount %%.LineCount2
+		%&% !%~1!.LineCount %%.LineCount
+		%&% !%~2!.LineCount %%.LineCount2
 		if !%%.LineCount! geq 1 (
 			if !%%.LineCount2! geq 1 (
-				!_C_Copy! !%~1!.Line[!%%.LineCount!] %%.Line
-				!_C_Copy! !%~2!.Line[1] %%.Line2
+				%&% !%~1!.Line[!%%.LineCount!] %%.Line
+				%&% !%~2!.Line[1] %%.Line2
 				set "!%~1!.Line[!%%.LineCount!]=!%%.Line!!%%.Line2!"
 			)
 		)
 		for /l %%i in (2 1 !%%.LineCount2!) do (
 			set /a %%.LineCount += 1
-			!_C_Copy! !%~2!.Line[%%i] !%~1!.Line[!%%.LineCount!]
+			%&% !%~2!.Line[%%i] !%~1!.Line[!%%.LineCount!]
 		)
-		!_C_Copy! %%.LineCount !%~1!.LineCount
-		!_C_Return! _
+		%&% %%.LineCount !%~1!.LineCount
+		%<-% _
 	)
-exit /b 0
+%-|%
 
-:STR_AppendVal _Str _Val -> _
+:STR_AppendVal &Str _Val
 	for %%. in (_L{!_G_LEVEL!}_) do (
-		!_C_Copy! !%~1!.LineCount %%.LineCount
+		%&% !%~1!.LineCount %%.LineCount
 		if "!%%.LineCount!" == "0" (
 			set "!%~1!.LineCount=1"
 			set %%.LineCount=1
 		)
 		if defined !%~1!.Line[!%%.LineCount!] (
-			!_C_Copy! !%~1!.Line[!%%.LineCount!] %%.LastLine
+			%&% !%~1!.Line[!%%.LineCount!] %%.LastLine
 			set "%%.LastLine=!%%.LastLine!%~2"
-			!_C_Copy! %%.LastLine !%~1!.Line[!%%.LineCount!]
+			%&% %%.LastLine !%~1!.Line[!%%.LineCount!]
 		) else (
 			set "%%.LastLine=%~2"
-			!_C_Copy! %%.LastLine !%~1!.Line[!%%.LineCount!]
+			%&% %%.LastLine !%~1!.Line[!%%.LineCount!]
 		)
-		!_C_Return! _
+		%<-% _
 	)
-exit /b 0
+%-|%
 
-:STR_AppendVar _Str _Var -> _
+:STR_AppendVar &Str _Var
 	for %%. in (_L{!_G_LEVEL!}_) do (
-		!_C_Copy! !%~1!.LineCount %%.LineCount
+		%&% !%~1!.LineCount %%.LineCount
 		if "!%%.LineCount!" == "0" (
 			set "!%~1!.LineCount=1"
 			set %%.LineCount=1
 		)
 		if defined !%~1!.Line[!%%.LineCount!] (
-			!_C_Copy! !%~1!.Line[!%%.LineCount!] %%.LastLine
+			%&% !%~1!.Line[!%%.LineCount!] %%.LastLine
 			set "%%.LastLine=!%%.LastLine!!%~2!"
-			!_C_Copy! %%.LastLine !%~1!.Line[!%%.LineCount!]
+			%&% %%.LastLine !%~1!.Line[!%%.LineCount!]
 		) else (
 			set "%%.LastLine=!%~2!"
-			!_C_Copy! %%.LastLine !%~1!.Line[!%%.LineCount!]
+			%&% %%.LastLine !%~1!.Line[!%%.LineCount!]
 		)
-		!_C_Return! _
+		%<-% _
 	)
-exit /b 0
+%-|%
